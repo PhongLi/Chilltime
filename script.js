3e2a9a8467afc759863a5da594361517099a5c56
PhongLi <41178601+PhongLi@users.noreply.github.com>
2020-04-04 17:09:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -356,7 +356,6 @@ $(function()
 
             albumName.text(currAlbum);
             trackName.text(currTrackName);
-		albumArt.find('img').attr('src', currArtwork);
             $('#album-art img').prop('src', bgArtworkUrl);
             $('.song').removeClass('playingSong');
             $('#song' + currIndex).addClass('playingSong');
