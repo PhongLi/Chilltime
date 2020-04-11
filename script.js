8650a1b2f76a8a9556f82a55a5172379419f2683
PhongLi <41178601+PhongLi@users.noreply.github.com>
2020-04-11 17:20:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,17 +21,7 @@ $(function()
 		buffInterval = null, tFlag = false;
 	
 	var playPreviousTrackButton = $('#play-previous'), playNextTrackButton = $('#play-next'), currIndex = -1;
-	function shuffle(a) {
-		var j, x, i;
-		for (i = a.length - 1; i > 0; i--) {
-			j = Math.floor(Math.random() * (i + 1));
-			x = a[i];
-			a[i] = a[j];
-			a[j] = x;
-		}
-		return a;
-	}
-	songs = shuffle(songs);
+	
 	var songs = [{
 		artist: "l.tus",
 		name: "lotus blossoms",
